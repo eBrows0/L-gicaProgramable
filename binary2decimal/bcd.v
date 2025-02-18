67ff9d97d6c5cd7115fcb42a809c091cd69136db
Gonzalo Flores García <95539637+eBrows0@users.noreply.github.com>
2025-02-18 09:56:25 -0600

    Update bcd.v

--- binary2decimal/bcd.v
+++ binary2decimal/bcd.v
@@ -1,1 +1,29 @@
+module bcd(
+	input[9:0] b_in,
+	output wire[6:0] un, dec, cent, mil
+);
+	wire[3:0] un_aux, dec_aux, cent_aux, mil_aux;
 
+	assign un_aux = b_in%10;
+	assign dec_aux = b_in%100/10;
+	assign cent_aux = b_in%1000/100;
+	assign mil_aux = b_in/1000;
+	
+	decoder uni_deco (
+		.deco_in(un_aux),
+		.deco_out(un)
+	);
+	decoder dec_deco (
+		.deco_in(dec_aux),
+		.deco_out(dec)
+	);
+	decoder cent_deco (
+		.deco_in(cent_aux),
+		.deco_out(cent)
+	);
+	decoder mil_deco (
+		.deco_in(mil_aux),
+		.deco_out(mil)
+	);
+
+endmodule
